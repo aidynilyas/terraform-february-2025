854304f6115ae70e1f29bdd4c789a450f5a11bab
aidynilyas <aidynilyas@gmail.com>
2025-04-11 01:48:18 +0000

    class5

---
+++ Class5/provider.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+
+variable region {
+    default = "us-east-1"
+    type = string
+}
